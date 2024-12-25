68debb691267f634647e474e5b74fe6d45a13d65
ManageMatic <ishanmahida123@gmail.com>
2024-12-25 22:02:39 +0530

    createing user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,27 +1,38 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../models/User')
-const { body, validationResult } = require('express-validator')
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
-// create user using: POST "api/auth/"
-router.post('/', [
+// Create user using: POST "api/auth/createuser"
+router.post('/createuser', [
     body('name').isLength({ min: 3 }),
     body('email').isEmail(),
     body('password').isLength({ min: 5 }),
-
-], (req,res)=>{
-    const errors = validationResult(req)
+], async (req, res) => {
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
+        return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    }).then(user => res.json(user))
-    .catch(err => {console.log(err)
-        res.json({ error: 'Enter a unique value', message: err.message})
-    })
-})
 
-module.exports = router
+    // Check if the user with this email already exists
+    try {
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ error: "User already exists with this email." });
+        }
+
+        // Create the user
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        });
+
+        res.json(user);
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Some error occured")
+    }
+});
+
+module.exports = router;
