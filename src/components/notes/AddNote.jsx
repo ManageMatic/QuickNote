fd28f1c35d8ded120ff6c19d543a31596548519d
ManageMatic <ishanmahida123@gmail.com>
2026-07-03 19:40:57 +0530

    chore: redeploy clean version without AI buttons

--- src/components/notes/AddNote.jsx
+++ src/components/notes/AddNote.jsx
@@ -1,3 +1,4 @@
+// Component for adding new notes
 import React, { useContext, useState } from 'react';
 import NoteContext from '../../context/notes/NoteContext';
 import ReactQuill from 'react-quill';
